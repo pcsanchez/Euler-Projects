07199a172a0ac72d56a9fd4473e9e9328a8a6bf3
Paco Sanchez <paco.sanchez.ram@gmail.com>
2018-06-30 08:34:35 -0500

    Finished solution for problem number 5 and updated README file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Euler Project
-This repository is dedicated to uploading solutions
-to Euler Programming Problems
+This repository is dedicated to uploading solutions to Euler Programming Problems.
+ **This is not a repository for you to copy solutions to the problems, but for consulting different ways of tackling the same problem** 
--- eulerFive.cpp
+++ eulerFive.cpp
@@ -3,16 +3,45 @@
 
 /*
 1: The number must end in a 0 in order to be divisible by 2,5,10
-2: 
 
+Divisibilities that depend on others:
+
+6:  The number must be divisible by 2 and 3
+12: The number must be divisible by 3 and 4
+14: The number must be divisible by 2 and 7
+15: The number must be divisible by 3 and 5
+18: The number must be divisible by 2 and 9
+20: The number must be divisible by 10
+
+Independent Divisibilities
+
+3,4,7,8,9,11,13,16,17,19
 */
+
 #include <iostream>
 
 using namespace std;
 
+bool divisible(int num){
+  if(num%3!=0||num%4!=0||num%7!=0||num%8!=0||num%9!=0||num%11!=0||num%13!=0||num%16!=0||num%17!=0||num%19!=0||num%20!=0)
+  return false;
+  else
+  return true;
+}
+
 int main(){
 
+bool found = false;
+int answer = 0;
 
+for(int i = 2520;!found;i+=10){
+  if(divisible(i)){
+    answer=i;
+    found = true;
+  }
+}
+
+cout << answer << endl;
 
   return 0;
 }
