8a8730eaa7ced967ef9579f7c31f54e090f630f3
Paco Sanchez <paco.sanchez.ram@gmail.com>
2018-06-29 19:25:27 -0500

    Small changes to program

--- eulerFive.cpp
+++ eulerFive.cpp
@@ -1,1 +1,18 @@
-//Program that calculates the smallest
+//Program that calculates the smallest positive number that can be evenly
+//divisible by all of the numbers from 1 to 20
+
+/*
+1: The number must end in a 0 in order to be divisible by 2,5,10
+2: 
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+
+  return 0;
+}
